bb068bdf1382509db4fd4c81172bbcc10024713d
Jasvinder <mrkaransingh3593@gmail.com>
2024-05-01 06:49:18 +0530

    fixed: copied summarized text

--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,13 @@
-// window.onload = () => {
-//     const element = document.createElement('div')
-//     element.setAttribute('class', 'loader block')
-//     document.body.setAttribute('class', 'page_loader')
-//     document.body.appendChild(element)
-//     const page = document.body.querySelector('.page')
-//     page.style.display = 'none'
-//     setTimeout(() => {
-//         document.body.removeChild(element)
-//         document.body.removeAttribute('class', 'page_loader')
-//         page.style.display = 'block'
-//     }, 2000);
-// };
+window.onload = () => {
+    const element = document.createElement('div')
+    element.setAttribute('class', 'loader block')
+    document.body.setAttribute('class', 'page_loader')
+    document.body.appendChild(element)
+    const page = document.body.querySelector('.page')
+    page.style.display = 'none'
+    setTimeout(() => {
+        document.body.removeChild(element)
+        document.body.removeAttribute('class', 'page_loader')
+        page.style.display = 'block'
+    }, 2000);
+};
